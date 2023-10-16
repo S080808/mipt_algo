10ee63bb92e2e49388a5fe392eb989ad17fc19e2
S080808 <0107suren@mail.ru>
2023-10-16 19:50:53 +0300

    my first commit

--- ProblemB.cpp
+++ ProblemB.cpp
@@ -1,22 +1,46 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
 int main() {
-  int ingredients_number = 8;
-  std::vector<int> ingredients = {2, 3, 4, 5, 2, 3, 5, 1};
-  std::vector<int> stack_of_ingredients;
-  int answer = 0;
-  stack_of_ingredients.push_back(ingredients[0]);
-  for (int ingredient = 1; ingredient < ingredients_number; ++ingredient) {
-    int current_ingredient = ingredients[ingredient];
-    if (current_ingredient > stack_of_ingredients.back()) {
-      stack_of_ingredients.push_back(current_ingredient);
-    }
-    int counter = 1;
-    while (!stack_of_ingredients.empty() && stack_of_ingredients.back() >= current_ingredient) {
-      answer = std::max(answer, counter * stack_of_ingredients.back());
-      stack_of_ingredients.pop_back();
+  int plants_number;
+  std::cin >> plants_number;
+
+  std::vector<double> loudness(plants_number);
+  for (int i = 0; i < plants_number; ++i) {
+    std::cin >> loudness[i];
+  }
+
+  std::vector<double> prefix_product_log(plants_number);
+  double log_of_product_of_loudness = 0;
+  for (int i = 0; i < plants_number; ++i) {
+    log_of_product_of_loudness += log(loudness[i]);
+    prefix_product_log[i] = log_of_product_of_loudness;  // боимся переполнения
+  }
+
+  int requests_number;
+  std::cin >> requests_number;
+
+  std::vector<std::pair<double, double>> requests_array(requests_number);
+  for (int i = 0; i < requests_number; ++i) {
+    std::cin >> requests_array[i].first >> requests_array[i].second;
+  }
+
+  for (int i = 0; i < requests_number; ++i) {
+    int left = requests_array[i].first;
+    int right = requests_array[i].second;
+    const int kPrecision = 10;
+    std::cout << std::fixed << std::setprecision(kPrecision);
+    if (left > 0) {
+      std::cout << exp(
+          1.0 / (right - left + 1) *
+          (prefix_product_log[right] - prefix_product_log[left - 1]));
+      std::cout << std::endl;
+    } else {
+      std::cout << exp(1.0 / (right - left + 1) * prefix_product_log[right]);
+      std::cout << std::endl;
     }
   }
-  std::cout << answer;
 }
